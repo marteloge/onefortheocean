f5d287d62200008f066a5b08b968cd1e032a7800
Marte Løge <marte.loge@gmail.com>
2020-04-18 15:59:57 -0400

    fix: move translate and burger to menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import React, { useState, useRef } from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 
-import { getMessages, SupportedLanguage, languages } from './i18n';
+import { getMessages, SupportedLanguage } from './i18n';
 import { useLocalStorage, useOnClickOutside } from './hooks';
 import { GlobalStyle } from './global';
 
 import Menu from './components/Menu/Menu';
-import Burger from './components/Burger/Burger';
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
 import Gratitude from './components/Gratitude/Gratitude';
 import About from './components/About/About';
-import Translate from './components/Translate/Translate';
 
 const App = () => {
   const [locale, setLocale] = useLocalStorage<SupportedLanguage>('lang', 'en');
@@ -33,16 +31,13 @@ const App = () => {
           <Route path="/thanks" component={Gratitude} />
         </Switch>
 
-        <div ref={node}>
-          <Burger open={open} setOpen={setOpen} />
-          <Menu open={open} locale={locale} setLocale={setLocale} />
-          <Translate
-            languages={Object.keys(languages)}
-            locale={locale}
-            setLocale={setLocale}
-            open={open}
-          ></Translate>
-        </div>
+        <Menu
+          ref={node}
+          open={open}
+          locale={locale}
+          setLocale={setLocale}
+          setOpen={setOpen}
+        ></Menu>
       </IntlProvider>
     </Router>
   );
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import { StyledMenu, StyledLink } from './Menu.styled';
 import { FormattedMessage } from 'react-intl';
 
-import { SupportedLanguage } from '../../i18n';
+import { SupportedLanguage, languages } from '../../i18n';
+
+import Translate from '../Translate/Translate';
+import Burger from '../Burger/Burger';
 
 export type Props = {
+  ref: RefObject<HTMLDivElement> | null;
   open: boolean;
   setLocale: Function;
   locale: SupportedLanguage;
+  setOpen: (v: boolean) => void;
 };
 
 const Menu = (props: Props) => {
   return (
-    <StyledMenu open={props.open}>
-      <StyledLink to="/" exact={true}>
-        <FormattedMessage id="route.home.title" />
-      </StyledLink>
-      <StyledLink to="/about">
-        <FormattedMessage id="route.about.title" />
-      </StyledLink>
-      <StyledLink to="/contact">
-        <FormattedMessage id="route.contact.title" />
-      </StyledLink>
-      <StyledLink to="/thanks">
-        <FormattedMessage id="route.gratitude.title" />
-      </StyledLink>
-    </StyledMenu>
+    <>
+      <Burger open={props.open} setOpen={props.setOpen} />
+
+      <Translate
+        languages={Object.keys(languages)}
+        locale={props.locale}
+        setLocale={props.setLocale}
+        open={props.open}
+      />
+
+      <StyledMenu open={props.open}>
+        <StyledLink to="/" exact={true}>
+          <FormattedMessage id="route.home.title" />
+        </StyledLink>
+        <StyledLink to="/about">
+          <FormattedMessage id="route.about.title" />
+        </StyledLink>
+        <StyledLink to="/contact">
+          <FormattedMessage id="route.contact.title" />
+        </StyledLink>
+        <StyledLink to="/thanks">
+          <FormattedMessage id="route.gratitude.title" />
+        </StyledLink>
+      </StyledMenu>
+    </>
   );
 };
 
