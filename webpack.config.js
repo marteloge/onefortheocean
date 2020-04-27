0178019bf52b4cb99773780639e754ee3aef5f8b
Marte Løge <marte.loge@gmail.com>
2020-04-27 15:10:24 -0400

    chore: add webpack prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
-const path = require("path");
-const isProd = process.env.NODE_ENV === "production";
+const path = require('path');
 
-module.exports = {
-  entry: "./src/index.tsx",
-  mode: isProd ? "production" : "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".css"],
-  },
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
+module.exports = (env) => {
+  const isProd = !!(env && env.production);
+
+  console.log(env, isProd);
+
+  return {
+    entry: './src/index.tsx',
+    mode: isProd ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.css'],
+    },
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+  };
 };
